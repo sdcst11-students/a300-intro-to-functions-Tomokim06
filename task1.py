ab97b51027e468e2b35eac22e63da21a9c5fc2ee
Tomokim06 <tomokim048@deltalearns.ca>
2023-10-24 12:51:32 -0700

    a300 done

--- task1.py
+++ task1.py
@@ -1,6 +1,19 @@
 def A():
+    print("Hello!")
 
 def B():
-
-
+    print("Greetings!")
 def C():
+    print("Hi!")
+
+q = 'Enter either "A", "B", or "C": '
+a = input(q)
+
+if a == "A":
+    A()
+
+if a == "B":
+    B()
+    
+if a == "C":
+    C()
--- task2.py
+++ task2.py
@@ -8,3 +8,26 @@ displays instructions and how to play
 game()
 plays the games
 """
+
+
+import random
+import time
+def title():
+    print("\nThis game is called guess the number.\n\nI will ask you to guess a number between 1 and 10 \nand you have to guess until you get the right number.\nIf you guess wrong, try again.")
+
+def game():
+    num = random.randint(1, 10)
+    guess = None
+
+    while guess != num:
+        guess = input("guess a number between 1 and 10: ")
+        guess = int(guess)
+
+        if guess == num:
+            print("congratulations! you won!")
+            break
+        else:
+            print("nope, sorry. try again!")
+title()
+time.sleep(10)
+game()
